558917744dcdbda6f448eb1123d530cf29cf456a
SarathLakshman <luxman@blueyonder.co.uk>
2020-09-18 23:53:15 +0100

    post css added

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  plugins: [['postcss-short', { prefix: 'x' }], 'postcss-preset-env']
+  plugins: [
+    ['postcss-short', { prefix: 'x' }],
+    'postcss-preset-env',
+    'precss',
+    'postcss-simple-vars',
+    'postcss-nested',
+    'autoprefixer'
+  ]
 };
